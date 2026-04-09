8194c56d2bc7198a89249218e630026f503207ee
Ádna Esthefani <adna.esthefani@discente.univasf.edu.br>
2026-04-09 00:23:00 -0300

    Create app.js

---
+++ Frontend/src/app.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Minha Plataforma de Monitoramento de Fermentação</h1>
+      <p>Bem-vindo à aplicação!</p>
+    </div>
+  );
+}
+
+export default App;
